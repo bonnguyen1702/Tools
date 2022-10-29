83cf41f6be5362b4c66660f5aec78d9e6218baf2
Duc Anh Nguyen <107803568+bonnguyen1702@users.noreply.github.com>
2022-10-29 11:07:47 +0700

    Update gitpull-all.sh

--- gitpull-all.sh
+++ gitpull-all.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+#Tool này chạy git pull ở local cho tất cả git repo trong /home/anhnguyen80/Desktop/source
 for i in /home/anhnguyen80/Desktop/source/*/.git; do ( echo $i; cd $i/..; git pull; ); done
