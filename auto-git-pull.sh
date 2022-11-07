295e3a07d917d62c1e0a725bc5e4fa0d6ac481ed
Duc Anh Nguyen <107803568+bonnguyen1702@users.noreply.github.com>
2022-11-07 14:34:44 +0700

    Update auto-git-pull.sh

--- auto-git-pull.sh
+++ auto-git-pull.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 #this script will automatically trigger git pull only for git repo
+#B1: Cài đặt shorcut <Ctrl+T> cho VS Code thực hiện mở terminal cho folder hiện tại
+#B2: Thêm script này vào file .bashrc: "bash ~/Desktop/BashScriptTools/auto-git-pull.sh"
+
 findgit=$(ls -a | grep -w .git)
 
 if [[ $findgit == .git ]];    
